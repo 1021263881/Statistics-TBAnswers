c96cf038d3550aec5125dc03efcee401b22ded01
1021263881 <1021263881@qq.com>
2018-03-03 07:37:45 +0800

    #

--- app/build/gen/com/fapple/tj/R.java
+++ app/build/gen/com/fapple/tj/R.java
@@ -36,17 +36,16 @@ public final class R {
         public static final int jumppagebutton=0x7f070003;
         public static final int jumppagetext=0x7f070005;
         public static final int lzlContent=0x7f07000c;
-        public static final int lzlDate=0x7f07000d;
         public static final int lzlID=0x7f07000a;
         public static final int lzlIcon=0x7f070009;
         public static final int lzlNickname=0x7f07000b;
-        public static final int mainLinearLayout=0x7f070011;
-        public static final int mainList=0x7f07000f;
-        public static final int mainScrollView=0x7f070010;
-        public static final int mainWebView=0x7f07000e;
-        public static final int personbutton=0x7f070014;
-        public static final int personid=0x7f070012;
-        public static final int personnickname=0x7f070013;
+        public static final int mainLinearLayout=0x7f070010;
+        public static final int mainList=0x7f07000e;
+        public static final int mainScrollView=0x7f07000f;
+        public static final int mainWebView=0x7f07000d;
+        public static final int personbutton=0x7f070013;
+        public static final int personid=0x7f070011;
+        public static final int personnickname=0x7f070012;
     }
     public static final class layout {
         public static final int actionbar=0x7f030000;
